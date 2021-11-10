82bb82bfe4be74e84e8301ef65f508c9cc5b0bbe
junhyeok <toughhyeok96@gmail.com>
2021-11-10 22:42:54 +0900

    Add logout button

--- js/greetings.js
+++ js/greetings.js
@@ -1,15 +1,20 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 const loginBtn = document.querySelector("#login-form input:last-child");
-const greeting = document.querySelector("#greeting");
+const greetingH1 = document.querySelector("#greeting h1");
+const greetingForm = document.querySelector("#greeting");
+const logoutBtn = document.querySelector("#greeting input");
 
 const HIDDEN_CLASSNAME = "hidden";
 const USERNAME_KEY = "username";
 const TOGLE_CLASSNAME = "loginBtn-toggle";
 
 function paintGreetings(username) {
-    greeting.innerText = `Hello ${username}`;
-    greeting.classList.remove(HIDDEN_CLASSNAME);
+    greetingH1.innerText = `Hello ${username}`;
+    greetingForm.classList.remove(HIDDEN_CLASSNAME);
+    greetingForm.addEventListener("click", onLogoutClick);
+    logoutBtn.addEventListener("mouseenter", onMouseEnter);
+    logoutBtn.addEventListener("mouseleave", onMouseELeave);
 }
 
 function onLoginSubmit(event) {
@@ -22,10 +27,23 @@ function onLoginSubmit(event) {
 
 function onMouseEnter(event){
     loginBtn.classList.add(TOGLE_CLASSNAME);
+    logoutBtn.classList.add(TOGLE_CLASSNAME);
 }
 
 function onMouseELeave(event){
     loginBtn.classList.remove(TOGLE_CLASSNAME);
+    logoutBtn.classList.remove(TOGLE_CLASSNAME);
+}
+
+function onLogoutClick(event){
+    // event.preventDefault();
+    greetingForm.classList.add(HIDDEN_CLASSNAME);
+    localStorage.removeItem(USERNAME_KEY);
+    loginInput.value = "";
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+    loginBtn.addEventListener("mouseenter", onMouseEnter);
+    loginBtn.addEventListener("mouseleave", onMouseELeave);
 }
 
 const savedUsername = localStorage.getItem(USERNAME_KEY);
@@ -39,4 +57,7 @@ if (savedUsername === null) {
 } else {
     // show the greetings
     paintGreetings(savedUsername);
+    greetingForm.addEventListener("click", onLogoutClick);
+    logoutBtn.addEventListener("mouseenter", onMouseEnter);
+    logoutBtn.addEventListener("mouseleave", onMouseELeave);
 }
